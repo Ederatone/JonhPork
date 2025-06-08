44495856791f7ff02813cbee964a9811281d489d
turbososyn <turbososyn@gmail.com>
2025-06-08 23:39:32 +0300

    Initial project setup and secure token handling

---
+++ src/bot_config.py
@@ -0,0 +1,13 @@
+import os
+import pytz
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if TOKEN is None:
+    raise ValueError("Не знайдено TELEGRAM_BOT_TOKEN. Переконайтесь, що у вас є .env файл з цим значенням.")
+
+TIMEZONE_STR = "Europe/Kyiv"
+TIMEZONE = pytz.timezone(TIMEZONE_STR)
